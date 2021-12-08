812ca6f578b02e7e9e09fbb783fa70fa4bb7bbcb
BosToken <faizdiandra@gmail.com>
2021-12-08 15:47:42 +0700

    change

--- read.h
+++ read.h
@@ -9,12 +9,15 @@ int showFunction()
     {
         for (y = 0; y < i; y++)
         {
-            if(strlen(nim) == 0)
+            if(strlen(nim[y]) == 0)
             {
                 i -= 1;
+                return 0;
             }
             else{
                 printf("Data Ke-%i\n\n", y + 1);
+                
+                // printf("Jumlah NIM : %i\n", nim[y]);
                 printf("NIM Mahasiswa : %s\n", nim[y]);
                 printf("Nama Mahasiswa : %s\n", nama[y]);
                 printf("Kelas Pemrograman Dasar : %s\n", kelas[y]);
